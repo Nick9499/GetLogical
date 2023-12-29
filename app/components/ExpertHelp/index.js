68c422a3eb9d33497fa5c8f647fce483dee766a8
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-12-30 00:18:36 +0530

    Added dashboard card

--- app/components/ExpertHelp/index.js
+++ app/components/ExpertHelp/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Typography } from "../../theme/themeRegistry";
 import ConvoCard from "./ConvoCard";
+import DashboardCard from "./DashboardCard";
 
 const ExpertHelp = () => {
   return (
@@ -9,12 +10,39 @@ const ExpertHelp = () => {
         <Typography
           color="blue-gray"
           variant="h5"
-          className="font-semibold text-2xl">
+          className="font-semibold text-2xl text-[#033474]">
           Get expert help anytime.
         </Typography>
       </div>
       <div className="mx-10 my-5">
-        <ConvoCard />
+        <div>
+          <ConvoCard />
+          <div className="mt-10">
+            <Typography
+              variant="h6"
+              className="font-semibold text-lg  text-[#033474]">
+              Receive 24hr support
+            </Typography>
+            <Typography>
+              Inbound support teams to answer whatever questions your customers
+              may ask.
+            </Typography>
+          </div>
+        </div>
+        <div>
+          <DashboardCard />
+          <div className="mt-10">
+            <Typography
+              variant="h6"
+              className="font-semibold text-lg  text-[#033474]">
+              Pay on performance
+            </Typography>
+            <Typography>
+              We’re so sure of our sales team, you only pay us when we get you
+              more sales.
+            </Typography>
+          </div>
+        </div>
       </div>
     </div>
   );
