68762e915d4f2cc936c591f0f79f7b9528dd4f27
Nick9499 <sarkarsubhajit9499@gmail.com>
2025-04-13 07:40:54 +0000

    Added Banner fr large screen

--- app/components/Hero/index.js
+++ app/components/Hero/index.js
@@ -13,47 +13,114 @@ const HeroHome = () => {
   return (
     <>
       <div
-        className="hidden md:flex bg-contain bg-bottom h-[80vh] w-full  items-center -mt-24  pl-24 relative "
+        className="hidden lg:flex min-w-screen w-full -mt-24  md:items-center  relative  "
         style={{
           background: ` url(/banner.svg)`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       >
-        <div class=" text-white">
-          <Typography variant="h1" className="font-semibold">
-            Elevate Your Business
-            <br />
-            with Logical.
-          </Typography>
-          <Typography className="mt-2 text-lg w-2/3 mb-8">
-            Let us turn every call into your next big win
-          </Typography>
+        <div className="px-[15%]  max-w-[80vw] flex flex-col gap-8 lg:mt-[20%] xl:mt-[10%]   ">
+          <div class=" text-white">
+            <Typography variant="h1" className="font-bold text-[4rem]  ">
+              Elevate Your Business with UpSales.
+            </Typography>
+            <Typography className="mt-2 text-2xl font-light ">
+              UpSales is committed to supporting existing customers while also
+              strategically engaging prospective clients to encourage
+              conversions.
+            </Typography>
+          </div>
+          <div className="pb-10">
+            <Card
+              className="mt-6 w-full rounded-[2.15rem] border border-white backdrop-blur-sm"
+              style={{
+                background:
+                  "linear-gradient(92deg, rgba(255, 255, 255, 0.30) 10.4%, rgba(255, 255, 255, 0.20) 88.1%)",
+                boxShadow: "0px 1.693px 1.693px 0px rgba(0, 0, 0, 0.25)",
+              }}
+            >
+              <CardBody className="flex gap-2 items-center">
+                <div className="flex flex-col gap-2">
+                  <Avatar src="/omar.svg" />
+                  <div className="flex gap-1">
+                    {[...Array(5)].map((_, index) => (
+                      <FaStar className="text-[#FBC02D] w-[.35rem] h-[.35rem] " />
+                    ))}
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2  text-white">
+                  <Typography className="font-semibold text-[1.75rem] leading-[1.875rem] ">
+                    Omar Eltobgui
+                  </Typography>
+                  <Typography className="font-normal text-[1.15rem] leading-[1.875rem] ">
+                    CEO/Founder OFactor Media Group
+                  </Typography>
+                  <Typography className="font-normal text-[1.15rem] ">
+                    "Switching to UpSales was the best thing we ever did. Their
+                    customer service and sales department are second to none.
+                    It’s true what they say, New Yorkers have that “X” factor."
+                  </Typography>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+        <div className="px-[2%]  w-[40rem] flex  items-center">
+          <Card
+            className="mt-6 w-full rounded-[2.25rem] border text-white border-white backdrop-blur-sm"
+            style={{
+              background:
+                "linear-gradient(92deg, rgba(255, 255, 255, 0.30) 10.4%, rgba(255, 255, 255, 0.20) 88.1%)",
+              boxShadow: "0px 1.693px 1.693px 0px rgba(0, 0, 0, 0.25)",
+            }}
+          >
+            <CardBody className="flex flex-col gap-3 ">
+              <div>
+                <Typography className="text-base font-semibold">24%</Typography>
+                <Typography className="text-[.5rem] leading-[.875rem] font-light ">
+                  Conversion Rate
+                </Typography>
+              </div>
+              <div>
+                <img src="/growth_vector.svg" className="w-full h-full" />
+              </div>
+              <div className="flex flex-col items-end">
+                <Typography className="text-base font-semibold">
+                  $180
+                </Typography>
+                <Typography className="text-[.5rem] leading-[.875rem] font-light">
+                  Average Order Value
+                </Typography>
+              </div>
+            </CardBody>
+          </Card>
         </div>
       </div>
       <div
-        className="flex flex-col md:hidden -mt-16  bg-bottom  w-screen  items-center pt-24 pb-20 px-4"
+        className="flex flex-col lg:hidden -mt-16 md:-mt-24  bg-bottom  w-screen  items-center pt-24 pb-20 px-4"
         style={{
           background: `url(/banner_xs.svg)`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
         }}
       >
-        <div class=" text-white">
+        <div class=" text-white md:mt-12">
           <Typography
             variant="h1"
             className="text-[1.8rem] font-semibold text-center"
           >
-            Elevate Your Business with Logical.
+            Elevate Your Business with UpSales.
           </Typography>
           <Typography className=" mt-3 px-2  text-[.875rem] font-normal text-center ">
-            Logicall is committed to supporting existing customers while also
+            UpSales is committed to supporting existing customers while also
             strategically engaging prospective clients to encourage conversions.
           </Typography>
         </div>
-        <div>
+        <div className="flex items-center">
           <Card
-            className="mt-6 w-full rounded-xl border border-white backdrop-blur-sm"
+            className="mt-6 w-full md:w-[40rem] rounded-xl border border-white backdrop-blur-sm"
             style={{
               background:
                 "linear-gradient(92deg, rgba(255, 255, 255, 0.30) 10.4%, rgba(255, 255, 255, 0.20) 88.1%)",
@@ -77,7 +144,7 @@ const HeroHome = () => {
                   CEO/Founder OFactor Media Group
                 </Typography>
                 <Typography className="font-normal text-[.5rem] leading-[.875rem]">
-                  "Switching to Logicall was the best thing we ever did. Their
+                  "Switching to UpSales was the best thing we ever did. Their
                   customer service and sales department are second to none. It’s
                   true what they say, New Yorkers have that “X” factor."
                 </Typography>
@@ -85,7 +152,7 @@ const HeroHome = () => {
             </CardBody>
           </Card>
         </div>
-        <div>
+        <div className="">
           <Card
             className="mt-6 w-full rounded-[1.25rem] border text-white border-white backdrop-blur-sm"
             style={{
--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -14,67 +14,14 @@ import {
   List,
   Popover,
   PopoverContent,
-  PopoverHandler
+  PopoverHandler,
 } from "../../theme/themeRegistry";
 import { Bars3Icon, PhoneIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-scroll";
 
 function NavList() {
   return (
-  
-      <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
-    <Typography variant="small" color="blue-gray" className="font-medium">
-      <ListItem className="flex items-center gap-2 py-2 pr-4">
-        <Link
-          to="service"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}>
-          Services
-        </Link>
-      </ListItem>
-    </Typography>
-    <Typography variant="small" color="blue-gray" className="font-medium">
-      <ListItem className="flex items-center gap-2 py-2 pr-4">
-        <Link
-          to="integrations"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}>
-          Integrations
-        </Link>
-      </ListItem>
-    </Typography>
-    <Typography variant="small" color="blue-gray" className="font-medium">
-      <ListItem className="flex items-center gap-2 py-2 pr-4">
-        <Link
-          to="lookup"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}>
-          Lookup
-        </Link>
-      </ListItem>
-    </Typography>
-    <Typography variant="small" color="blue-gray" className="font-medium">
-      <ListItem className="flex items-center gap-2 py-2 pr-4">
-        <Link href="/">Testimonials</Link>
-      </ListItem>
-    </Typography>
-  </List>
-    
-    
-  );
-}
-
-function NavListForSmallDevice({ setOpenNav }) {
-  return (
-   
-        <List onBlur={() => setOpenNav(false)}
-         className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1 ">
+    <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
       <Typography variant="small" color="blue-gray" className="font-medium">
         <ListItem className="flex items-center gap-2 py-2 pr-4">
           <Link
@@ -83,7 +30,7 @@ function NavListForSmallDevice({ setOpenNav }) {
             smooth={true}
             offset={-70}
             duration={500}
-            onClick={() => setOpenNav(false)}>
+          >
             Services
           </Link>
         </ListItem>
@@ -96,33 +43,65 @@ function NavListForSmallDevice({ setOpenNav }) {
             smooth={true}
             offset={-70}
             duration={500}
-            onClick={() => setOpenNav(false)}>
+          >
             Integrations
+          </Link>
+        </ListItem>
+      </Typography>
+
+      <Typography variant="small" color="blue-gray" className="font-medium">
+        <ListItem className="flex items-center gap-2 py-2 pr-4">
+          <Link href="/">Testimonials</Link>
+        </ListItem>
+      </Typography>
+    </List>
+  );
+}
+
+function NavListForSmallDevice({ setOpenNav }) {
+  return (
+    <List
+      onBlur={() => setOpenNav(false)}
+      className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1 "
+    >
+      <Typography variant="small" color="blue-gray" className="font-medium">
+        <ListItem className="flex items-center gap-2 py-2 pr-4">
+          <Link
+            to="service"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            onClick={() => setOpenNav(false)}
+          >
+            Services
           </Link>
         </ListItem>
       </Typography>
       <Typography variant="small" color="blue-gray" className="font-medium">
         <ListItem className="flex items-center gap-2 py-2 pr-4">
           <Link
-            to="lookup"
+            to="integrations"
             spy={true}
             smooth={true}
             offset={-70}
             duration={500}
-            onClick={() => setOpenNav(false)}>
-            Lookup
+            onClick={() => setOpenNav(false)}
+          >
+            Integrations
           </Link>
         </ListItem>
       </Typography>
+
       <Typography variant="small" color="blue-gray" className="font-medium">
         <ListItem
           className="flex items-center gap-2 py-2 pr-4"
-          onClick={() => setOpenNav(false)}>
+          onClick={() => setOpenNav(false)}
+        >
           <Link href="/">Testimonials</Link>
         </ListItem>
       </Typography>
     </List>
-   
   );
 }
 
@@ -142,8 +121,10 @@ export function GetLogicCallNavbar() {
   }, []);
 
   return (
-    <Navbar  className="mx-auto w-[90vw] md:w-[70vw]   px-4 py-[.01rem] md:py-2 sticky
-     mt-4 md:mt-7 rounded-[4.313rem] top-2 z-10 ">
+    <Navbar
+      className="mx-auto w-[90vw] md:w-[70vw]   px-4 py-[.01rem] md:py-2 sticky
+     mt-4 md:mt-7 rounded-[4.313rem] top-2 z-10 "
+    >
       <div className="flex items-center justify-between text-blue-gray-900">
         <Link href="/">
           <div className=" max-w-[2rem] max-h-[2rem] md:max-w-[70px] md:max-h-[70px]">
@@ -154,26 +135,33 @@ export function GetLogicCallNavbar() {
           <NavList />
         </div>
         <div className="hidden gap-2 lg:flex">
-          <Button variant="filled" className="capitalize bg-[#E88E46] 
-          rounded-[2.188rem] text-base font-medium " onClick={handleCallUs}>
+          <Button
+            variant="filled"
+            className="capitalize bg-[#E88E46] 
+          rounded-[2.188rem] text-base font-medium "
+            onClick={handleCallUs}
+          >
             Contact Us
           </Button>
         </div>
-        
+
         <IconButton
           variant="text"
           color="blue-gray"
           className="lg:hidden"
-          onClick={() => setOpenNav(!openNav)}>
+          onClick={() => setOpenNav(!openNav)}
+        >
           {openNav ? (
             <XMarkIcon className="h-6 w-6" strokeWidth={2} />
           ) : (
             <Bars3Icon className="h-6 w-6" strokeWidth={2} />
           )}
         </IconButton>
-        
       </div>
-      <Collapse className="fixed top-[3.8rem] left-0 w-full z-50 bg-white rounded-2xl shadow-md"  open={openNav}>
+      <Collapse
+        className="fixed top-[3.8rem] left-0 w-full z-50 bg-white rounded-2xl shadow-md"
+        open={openNav}
+      >
         <NavListForSmallDevice setOpenNav={setOpenNav} />
       </Collapse>
     </Navbar>
