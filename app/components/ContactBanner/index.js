89b347b211841a6d9368a4ff674838339a2f33db
Nick9499 <sarkarsubhajit9499@gmail.com>
2025-04-19 13:13:28 +0000

    Added contact us large screen

--- app/components/ContactBanner/index.js
+++ app/components/ContactBanner/index.js
@@ -4,7 +4,7 @@ const ContactBanner = () => {
   return (
     <>
       <div
-        className="hidden lg:flex min-w-screen w-full -mt-24  md:items-center  relative  "
+        className="hidden lg:flex min-w-screen w-full -mt-24 items-center text-center  relative  "
         style={{
           background: ` url(/banner.svg)`,
           backgroundRepeat: "no-repeat",
@@ -12,7 +12,24 @@ const ContactBanner = () => {
           backgroundPosition: "center",
         }}
       >
-        <Typography>Get in touch with us</Typography>
+        <div className="lg:mt-[20%] xl:mt-[10%] flex flex-col gap-10 pb-16 md:px-20 justify-center items-center w-full">
+          <div className="text-white md:mt-12 flex flex-col gap-10 ">
+            <Typography
+              variant="h1"
+              className="text-[4rem] leading-[0.24rem] font-semibold text-center"
+            >
+              Get in touch with us
+            </Typography>
+            <Typography className="mt-3 px-2  text-[2.25rem] leading-[3.75rem] font-normal text-center">
+              We’re ready to help you tackle what’s holding you back
+            </Typography>
+          </div>
+          <div>
+            <Button className="bg-[#212D3F] text-xl leading-[1.094rem]  rounded-3xl normal-case">
+              Book a Call
+            </Button>
+          </div>
+        </div>
       </div>
       <div
         className="flex flex-col gap-10 lg:hidden -mt-16 md:-mt-24  bg-bottom  w-screen  items-center pt-24 pb-20 px-4"
